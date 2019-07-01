99050d5453c5b18d9838dfb32c6ff4e9690e72f5
Emmanuelle <emmanuelle.em.menard@gmail.com>
2019-07-01 14:29:25 +0200

    Creating Project

---
+++ app/src/main/java/com/menard/mynews/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.menard.mynews.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.menard.mynews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
