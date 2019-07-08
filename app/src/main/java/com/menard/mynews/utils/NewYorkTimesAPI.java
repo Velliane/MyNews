6a7be17598ffd2eeb61104d28efafc575cf66348
Emmanuelle <emmanuelle.em.menard@gmail.com>
2019-07-08 17:27:29 +0200

    Add GET for MostPopular and Searched

--- app/src/main/java/com/menard/mynews/utils/NewYorkTimesAPI.java
+++ app/src/main/java/com/menard/mynews/utils/NewYorkTimesAPI.java
@@ -1,5 +1,7 @@
 package com.menard.mynews.utils;
 
+import com.menard.mynews.model.most_popular.ArticleMostPopular;
+import com.menard.mynews.model.search.ArticleSearched;
 import com.menard.mynews.model.top_stories.ArticleTopStories;
 
 import retrofit2.Call;
@@ -15,6 +17,12 @@ public interface NewYorkTimesAPI {
     @GET("svc/topstories/v2/{subject}.json")
     Call<ArticleTopStories> getTopStories(@Path("subject")String subject, @Query("api-key") String value);
 
+    @GET("svc/mostpopular/v2/viewed/1.json")
+    Call<ArticleMostPopular> getMostPopular(@Query("api-key") String value);
+
+    @GET("svc/search/v2/articlesearch.json")
+    Call<ArticleSearched> getSearched(@Query("q") String section, @Query("api-key") String value);
+
 
    Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://api.nytimes.com/svc/topstories/v2/")
@@ -23,7 +31,7 @@ public interface NewYorkTimesAPI {
 
 
 
-    //Call<ArticleTopStories> resultCall= retrofit.create(NewYorkTimesAPI.class).getTopStories("arts", "yHD5uUtRQngsZLyVUwKbVKSxvEihrB0m");
+    //Call<ArticleTopStories> resultCall= retrofit.create(NewYorkTimesAPI.class).getTopStories("home", "yHD5uUtRQngsZLyVUwKbVKSxvEihrB0m");
         //resultCall.enqueue(new Callback<ArticleTopStories>() {
         //@Override
         //public void onResponse(Call<ArticleTopStories> call, Response<ArticleTopStories> response) {
