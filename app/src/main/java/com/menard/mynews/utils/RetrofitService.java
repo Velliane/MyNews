64bfec0b5f1da7daa931038c83f586e3e921fbcc
Emmanuelle <emmanuelle.em.menard@gmail.com>
2019-07-31 17:09:22 +0200

    Creating drawable folder for all resolution

--- app/src/main/java/com/menard/mynews/utils/RetrofitService.java
+++ app/src/main/java/com/menard/mynews/utils/RetrofitService.java
@@ -8,10 +8,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitService {
 
     public Retrofit getRetrofit(){
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(logging);
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
+
 
         return new Retrofit.Builder()
                 .baseUrl("http://api.nytimes.com/")
